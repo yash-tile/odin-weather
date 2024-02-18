567ec108a4fc569f1240ff281a2d309870d120ff
yash-tile <yash.tile@indexnine.com>
2024-02-18 23:14:20 +0530

    fetch and parse current weather data

---
+++ script.js
@@ -0,0 +1,51 @@
+const apiKey = "126ad1e6af5f45d8b5954840241602";
+const baseUri = "https://api.weatherapi.com/v1/";
+const locationId = "1129302";
+
+// to parse the data of current weather from api
+function parseCurrentWeather(data){
+
+    // extracting location properties and then combining
+    const { name, region, country } = data.location
+    const location =  `${name}, ${region}, ${country}`;
+
+    // extracting current weather related properties
+    const { last_updated, temp_c, temp_f, condition: { text: condition_text, icon: condition_icon }, wind_mph, wind_kph, humidity, feelslike_c, feelslike_f } = data.current;
+    return {
+        location, 
+        last_updated,
+        condition_icon, 
+        condition_text,
+        temp_c,
+        temp_f,
+        feelslike_c, 
+        feelslike_f,
+        humidity,
+        wind_kph,
+        wind_mph
+    }
+}
+
+// to render weather data to ui
+function displayCurrentWeather(data){
+    console.log(data);
+}
+
+// to get current weather information
+async function getCurrentWeather(locationId){
+    const currentWeatherUri = `${baseUri}current.json?key=${apiKey}&q=id:${locationId}`;
+    const response = await fetch(currentWeatherUri);
+    // validate response from promise
+    if(!response.ok){
+        return new Error(`Error! ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data;
+}
+
+getCurrentWeather(locationId)
+    .then((data) => {
+        const parsedData = parseCurrentWeather(data);
+        displayCurrentWeather(parsedData);
+    })
+    .catch((err) => console.log(err));
