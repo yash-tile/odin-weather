730bb54418b300079be4b18f9cc4be466a7692c2
yash-tile <yash.tile@indexnine.com>
2024-02-21 12:27:33 +0530

    intial release

--- script.js
+++ script.js
@@ -11,16 +11,15 @@ const airQualityCategories  = {
     6: "Hazardous"
 };
 
-// to parse the data of current weather from api
 function parseCurrentWeather(data){
 
-    // extracting location properties and then combining
+    // extracting location properties and then combining for a readable location name
     const { name, region, country } = data.location
     const location =  `${name}, ${region}, ${country}`;
 
-    // extracting current weather related properties
-    const { last_updated, temp_c, temp_f, condition: { text: condition_text, icon: condition_icon } } = data.current;
-    const { wind_mph, wind_kph, humidity, feelslike_c, feelslike_f } = data.current;
+    // extracting required current weather related properties
+    const { last_updated, temp_c, condition: { text: condition_text, icon: condition_icon } } = data.current;
+    const { wind_kph, humidity, feelslike_c } = data.current;
     const { air_quality: { "us-epa-index": indexValue } } = data.current;
     
     return {
@@ -29,39 +28,30 @@ function parseCurrentWeather(data){
         condition_icon, 
         condition_text,
         temp_c,
-        temp_f,
         feelslike_c, 
-        feelslike_f,
         humidity,
         wind_kph,
-        wind_mph,
         indexValue
     }
 }
 
-// to render weather data to ui
 function displayCurrentWeather(data){
-    // make weather container visible
     document.getElementById('weather-container').style.display = 'block';
 
-    // select html elements
     const location = document.getElementById("location");
     const lastUpdated = document.getElementById("last-updated");
     const conditionImg = document.getElementById("condition-img");
     const temp = document.getElementById("temp");
-    const tempUnit = document.getElementById("temp-unit");
     const condtionText = document.getElementById("condition-text");
     const feelslikeTemp = document.getElementById("feelslike-temp");
     const humidity = document.getElementById("humidity");
     const windspeed = document.getElementById("windspeed");
     const aqi = document.getElementById("aqi");
 
-    // render data to selected elements
     location.textContent = data.location;
     lastUpdated.textContent = data.last_updated;
     conditionImg.src = data.condition_icon; 
-    temp.textContent = data.temp_c;
-    tempUnit.textContent = "°C";
+    temp.textContent = Math.round(data.temp_c);
     condtionText.textContent = data.condition_text;
     feelslikeTemp.textContent = data.feelslike_c;
     humidity.textContent = data.humidity;
@@ -69,30 +59,34 @@ function displayCurrentWeather(data){
     aqi.textContent = airQualityCategories[data.indexValue];
 }
 
-// async code to make api requests
 async function makeRequest(uri){
     const response = await fetch(uri);
     // validate response from promise
     if(!response.ok){
-        return new Error(`Error! ${response.statusText}`);
+        throw new Error(`Error! ${response.status}`);
     }
     const data = await response.json();
     return data;
 }
 
-// to get current weather information
 function getCurrentWeather(searchTerm){
     const currentWeatherUri = `${baseUri}current.json?key=${apiKey}&aqi=yes&q=${searchTerm}`;
     makeRequest(currentWeatherUri)
         .then((data) => {
+            console.log(data);
             const parsedData = parseCurrentWeather(data);
             displayCurrentWeather(parsedData);
         })
-        .catch((err) => console.log(err));
+        .catch((err) => {
+            console.log(err.message);
+            const weatherContainer = document.getElementById("weather-container");
+            weatherContainer.style.display = 'block';
+            weatherContainer.innerHTML = "Bad request! Please enter a valid city name.";
+        });
 }
 
  function searchHandler(){
     const inputBox = document.getElementById("input-box"); 
     let searchTerm = inputBox.value;
     getCurrentWeather(searchTerm);
- }
+}
